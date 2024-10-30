1ba6b742794dcb8973f07f7a8cee3a5a2deea61b
ChrisM345 <chrism.12277@gmail.com>
2024-10-29 19:38:51 -0500

    Added file route

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 const indexRoute = require("./routes/indexRoute");
+const fileRoute = require("./routes/fileRoute");
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRoute);
+app.use("/file", fileRoute);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
