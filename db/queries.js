1fb0896969e667a672e8449e1cc8db0ea560ee53
ChrisM345 <chrism.12277@gmail.com>
2024-10-29 19:38:03 -0500

    Additional queries for setting admin role and creating and getting folders

--- db/queries.js
+++ db/queries.js
@@ -30,8 +30,47 @@ async function createUser(username, password) {
   });
 }
 
+async function setAdmin(id) {
+  const updateUser = await prisma.Users.update({
+    where: {
+      id: id,
+    },
+    data: {
+      role: "ADMIN",
+    },
+  });
+}
+
+async function createFolder(userid, name) {
+  const createFolder = await prisma.Folder.create({
+    data: {
+      name: name,
+      user: {
+        connect: {
+          id: userid,
+        },
+      },
+    },
+  });
+}
+
+async function getFolders(user) {
+  if (user) {
+    const folders = await prisma.Folder.findMany({
+      where: {
+        usersId: user.id,
+      },
+    });
+
+    return folders;
+  }
+}
+
 module.exports = {
   getUser,
   getUserById,
   createUser,
+  setAdmin,
+  createFolder,
+  getFolders,
 };
