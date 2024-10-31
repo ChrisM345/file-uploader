d029c4793fb69ae6134179a29f80bd77f83b27da
ChrisM345 <chrism.12277@gmail.com>
2024-10-30 19:43:16 -0500

    Additional routes for uploading files

--- routes/fileRoute.js
+++ routes/fileRoute.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/fileController");
 
-router.get("/upload", controller.getFileUpload);
 router.get("/createFolder", controller.getCreateFolder);
 router.post("/createFolder", controller.postCreateFolder);
+router.get("/:folderName", controller.folderView);
+router.get("/:folderName/uploadFile", controller.getFileUpload);
+router.post("/:folderName/uploadFile", controller.postFileUpload);
 
 module.exports = router;
