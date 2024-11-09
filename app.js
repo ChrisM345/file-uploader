88b2f31da2eebe04690298a409efae0935c18e3a
ChrisM345 <chrism.12277@gmail.com>
2024-11-09 16:26:55 -0600

    using prisma session store

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 8000;
 const path = require("node:path");
 
 const session = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
 const passport = require("passport");
 
 app.set("views", path.join(__dirname, "views"));
@@ -12,7 +14,22 @@ app.set("view engine", "ejs");
 const indexRoute = require("./routes/indexRoute");
 const fileRoute = require("./routes/fileRoute");
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+// app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    secret: "a santa at nasa",
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
 app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRoute);
