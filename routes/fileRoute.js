8ef5a81b9be39940fa54a2c5bfd446264ab67c3a
ChrisM345 <chrism.12277@gmail.com>
2024-11-09 16:26:18 -0600

    using multer for file uploads

--- routes/fileRoute.js
+++ routes/fileRoute.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/fileController");
 
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 router.get("/createFolder", controller.getCreateFolder);
 router.post("/createFolder", controller.postCreateFolder);
 router.get("/:folderName", controller.folderView);
 router.get("/:folderName/uploadFile", controller.getFileUpload);
-router.post("/:folderName/uploadFile", controller.postFileUpload);
+router.post("/:folderName/uploadFile", upload.single("fileData"), controller.postFileUpload);
 router.post("/:folderName/delete", controller.postDeleteFolder);
 router.post("/:folderName/:filename/id/:id/delete", controller.postDeleteFile);
 
