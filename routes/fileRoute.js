fb2751516705ff359bc52f080b58649ce33fcc3b
ChrisM345 <chrism.12277@gmail.com>
2024-11-10 22:35:30 -0600

    Added file details and file download route

--- routes/fileRoute.js
+++ routes/fileRoute.js
@@ -9,9 +9,14 @@ const upload = multer({ storage: storage });
 router.get("/createFolder", controller.getCreateFolder);
 router.post("/createFolder", controller.postCreateFolder);
 router.get("/:folderName", controller.folderView);
-router.get("/:folderName/uploadFile", controller.getFileUpload);
+
 router.post("/:folderName/uploadFile", upload.single("fileData"), controller.postFileUpload);
+router.post("/:folderName/:renameFolder", controller.postRenameFolder);
+router.get("/:folderName/uploadFile", controller.getFileUpload);
+
 router.post("/:folderName/delete", controller.postDeleteFolder);
-router.post("/:folderName/:filename/id/:id/delete", controller.postDeleteFile);
+router.post("/:folderName/:fileName/id/:id/delete", controller.postDeleteFile);
+router.get("/:folderName/:fileName", controller.getFileDetails);
+router.get("/:folderName/:fileName", controller.getDownloadFile);
 
 module.exports = router;
